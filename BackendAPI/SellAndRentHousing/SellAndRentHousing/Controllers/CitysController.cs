8dd686e4a180df66e8bec94c7bd2a6d6bf33ca34
MinhTran7959 <128154976+MinhTran7959@users.noreply.github.com>
2024-02-11 14:51:38 +0700

    Up_Create_API

---
+++ BackendAPI/SellAndRentHousing/SellAndRentHousing/Controllers/CitysController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SellAndRentHousing.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CitysController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetCity()
+        {
+            return new string[] { "Atlanta", "New York" };
+        }
+
+        [HttpGet("{id}")]
+        public string get(int id)
+        {
+            return  "Atlanta" ;
+        }
+
+    } 
+}
