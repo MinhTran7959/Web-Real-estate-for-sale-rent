74b810f879580d05b6b794c85adacc3c1d7d2420
MinhTran7959 <128154976+MinhTran7959@users.noreply.github.com>
2024-02-16 15:23:50 +0700

    up_Angular CLI_16

--- Fontend/src/app/Property/property-datail/property-details-resolver.service.ts
+++ Fontend/src/app/Property/property-datail/property-details-resolver.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable, catchError, of } from 'rxjs';
 import { Property } from 'src/app/model/property';
 import { HousingService } from 'src/app/services/housing.service';
@@ -7,7 +7,7 @@ import { HousingService } from 'src/app/services/housing.service';
 @Injectable({
   providedIn: 'root'
 })
-export class PropertyDetailResolverService implements Resolve<Property> {
+export class PropertyDetailResolverService  {
 
   constructor(private router: Router,  private housingService: HousingService) { }
   
